ee1d896993c673ff9e13f0efc36e2f06f32cb70a
Kschademan <mr.kyle.schademan@gmail.com>
2020-09-08 18:14:59 -0700

    set up rough frame work for a binary star system program in python

---
+++ TwoStars(AppendixK).py
@@ -0,0 +1,9 @@
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#create variable storage for initial conditions and check for valid inputs
+
+#create algorithms for how to step through the calculations
+
+#create output methods (graphs, txt file, ect.)
